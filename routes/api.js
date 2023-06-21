f129695197b5ba41c8f5f3f23b7237864ee4d1f6
KimSH39 <sooooooohyeon5@naver.com>
2023-06-21 20:55:57 +0900

    test commit

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const apiRouter = require('express').Router()
 
-apiRouter.post('/sayHello', function (req, res) {
+apiRouter.post('/controlbulb', function (req, res) {
   const { userRequest } = req.body
   const utterance = userRequest.utterance
 
@@ -10,7 +10,47 @@ apiRouter.post('/sayHello', function (req, res) {
       outputs: [
         {
           simpleText: {
-            text: utterance,
+            text: 'bulb: ' + utterance,
+          },
+        },
+      ],
+    },
+  }
+
+  res.status(200).send(responseBody)
+})
+
+apiRouter.post('/controlair', function (req, res) {
+  const { userRequest } = req.body
+  const utterance = userRequest.utterance
+
+  const responseBody = {
+    version: '2.0',
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: 'air: ' + utterance,
+          },
+        },
+      ],
+    },
+  }
+
+  res.status(200).send(responseBody)
+})
+
+apiRouter.post('/controlmonitor', function (req, res) {
+  const { userRequest } = req.body
+  const utterance = userRequest.utterance
+
+  const responseBody = {
+    version: '2.0',
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: 'monitor: ' + utterance,
           },
         },
       ],
