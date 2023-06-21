a1488f613f9da43c35bca449da9b7a1a50bbc9bb
KimSH39 <sooooooohyeon5@naver.com>
2023-06-21 18:31:28 +0900

    test commit

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,14 @@
 const apiRouter = require('express').Router()
 
+function remote(req, res) {
+  body = request.get_json()
+  console.log(body)
+}
+
 apiRouter.post('/sayHello', function (req, res) {
   console.log(req.body)
+
+  body = request.get_json()
 
   const responseBody = {
     version: '2.0',
@@ -9,7 +16,7 @@ apiRouter.post('/sayHello', function (req, res) {
       outputs: [
         {
           simpleText: {
-            text: "hello I'm Ryan",
+            text: body,
           },
         },
       ],
@@ -18,5 +25,7 @@ apiRouter.post('/sayHello', function (req, res) {
 
   res.status(200).send(responseBody)
 })
+
+apiRouter.post('/', remote)
 
 module.exports = apiRouter
